0f1b215778a77b73977f41339c17caa070f80270
DevasaniNaveen <devasaninawin@gmail.com>
2025-08-06 10:07:23 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -7,8 +7,19 @@ import javax.servlet.http.*;
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
+        
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang=\"en\">");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF-8\">");
+        out.println("<title>Simple Java Web App</title>");
+        out.println("</head>");
+        out.println("<body>");
         out.println("<h1>Hello from a simple Java Web App!</h1>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
